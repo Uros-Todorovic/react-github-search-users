7d6eda974b27d9f67334eca57c63b386601cd1ad
Uroš Todorović <todorovic.urosh@gmail.com>
2022-12-06 11:02:19 +0100

    updated pie chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,7 +6,24 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
 	const { repos } = useGlobalContext();
 
-	const chartData = [
+	let languages = repos.reduce((total, item) => {
+		const { language } = item;
+		if (!language) return total;
+		if (!total[language]) {
+			total[language] = { label: language, value: 1 };
+		} else {
+			total[language] = { ...total[language], value: total[language].value + 1 };
+		}
+		return total;
+	}, {});
+
+	languages = Object.values(languages)
+		.sort((a, b) => {
+			return b.value - a.value;
+		})
+		.slice(0, 5);
+
+	/* 	const chartData = [
 		{
 			label: 'HTML',
 			value: '13',
@@ -19,12 +36,13 @@ const Repos = () => {
 			label: 'Javascript',
 			value: '80',
 		},
-	];
+	]; */
 
 	return (
 		<section className="section">
 			<Wrapper className="section-center">
-				<ExampleChart data={chartData} />
+				{/* <ExampleChart data={chartData} /> */}
+				<Pie3D data={languages} />
 			</Wrapper>
 		</section>
 	);
