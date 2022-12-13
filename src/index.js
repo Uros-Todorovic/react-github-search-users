c965a4dbcbf3676195d7b22e15c8f7ff308cb536
Uroš Todorović <todorovic.urosh@gmail.com>
2022-12-13 14:51:52 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import * as serviceWorker from './serviceWorker';
 import { AppProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// dev-qi4ss6o4dds53a8w.us.auth0.com
+// SdnVasB294bbRtBePB9iilOagusAS4En
+
 ReactDOM.render(
 	<React.StrictMode>
-		<AppProvider>
-			<App />
-		</AppProvider>
+		<Auth0Provider
+			domain="dev-qi4ss6o4dds53a8w.us.auth0.com"
+			clientId="SdnVasB294bbRtBePB9iilOagusAS4En"
+			redirectUri={window.location.origin}
+			cacheLocation="localstorage"
+		>
+			<AppProvider>
+				<App />
+			</AppProvider>
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
